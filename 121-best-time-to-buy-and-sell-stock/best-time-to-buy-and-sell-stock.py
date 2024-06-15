19a4d6fd9a6b099e1cdd75f509f4dc57761c7929
Aarushi Shah <17842323+aarushis18@users.noreply.github.com>
2024-06-14 22:25:32 -0400

    Time: 763 ms (39.39%) | Memory: 27 MB (97.23%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
+        cheapest_price = prices[0] 
         max_profit = 0
         
         for price in prices[1:]:
-            max_profit = max(max_profit, price - min_price)
-            min_price = min(price, min_price)
+            max_profit = max(max_profit, price - cheapest_price) 
+            cheapest_price = min(price, cheapest_price) # updates cheapest_price if current price is cheaper
             
         return max_profit
+
+        # O(N) for time complexity, since we run through each price in prices atleast once
+        # O(1) for memory, since we only keep track of the cheapest price, and the maximum profit
