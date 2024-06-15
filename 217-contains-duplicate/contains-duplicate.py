87856144f6eaee045384bfaae2bb31288f4230c4
Aarushi Shah <17842323+aarushis18@users.noreply.github.com>
2024-06-15 00:53:22 -0400

    Time: 417 ms (53.60%) | Memory: 31.9 MB (56.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,14 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        unique_nums = set() # using a set since we only want each value in there once
+        set_of_nums = set(nums)
 
-        for num in nums:
-            if num in unique_nums:
-                return True
+        if len(set_of_nums) == len(nums):
+            return False
+        else:
+            return True
 
-            unique_nums.add(num)
-        
-        return False
-
-        # O(N) for time complexity since we go through each value in nums once
-        # O(N) for memory since we create a map with values from nums
+        # O(1) for time complexity since we go through each value in nums once
+        # O(1) for memory since we create a map with values from nums
