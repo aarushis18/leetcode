c05b6932ddff3a72335ad931384094fccf7882ca
Aarushi Shah <17842323+aarushis18@users.noreply.github.com>
2024-07-24 19:34:29 -0400

    Time: 36 ms (53.24%) | Memory: 16.5 MB (20.22%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -7,3 +7,6 @@ class Solution:
                 count += 1
 
         return count
+
+        # O(1) time complexity since we always only go through 32 bits
+        # O(1) space complexity since we are only using simple variables, no data structures
