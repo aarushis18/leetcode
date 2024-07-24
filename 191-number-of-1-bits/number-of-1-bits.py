f556958949d6ff32cb5b4e56f7bf5297425bce2e
Aarushi Shah <17842323+aarushis18@users.noreply.github.com>
2024-07-24 19:24:27 -0400

    Time: 35 ms (62.45%) | Memory: 16.4 MB (62.36%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -5,10 +5,5 @@ class Solution:
         for i in range(32):
             if (n >> i) & 1:
                 count += 1
-        
+
         return count
-
-        # O(1) time complexity since we will always only iterate through 32 bits at most
-        # O(1) space complexity since it remains constant regardless of the number of bits
-
-        
