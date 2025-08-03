bb05b6936210fcbb3c1e8929bd84760237f30179
Aarushi Shah <17842323+aarushis18@users.noreply.github.com>
2025-08-02 22:05:18 -0400

    Time: 11 ms (23.60%) | Memory: 24.1 MB (20.20%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        for num in nums:                           # ❶ build freq map
-            count[num] = count.get(num, 0) + 1
+        freq = {}
 
-        # --- bucket array indexed by frequency ---
+        for num in nums:                           
+            freq[num] = freq.get(num, 0) + 1
+
+        # create an array of arrays with the freq of values
         buckets = [[] for _ in range(len(nums) + 1)]
-        for num, c in count.items():               # ❷ fill buckets
+        for num, c in freq.items():  
             buckets[c].append(num)
-
-        res = []
-        for freq in range(len(buckets) - 1, 0, -1):
-            for num in buckets[freq]:
-                res.append(num)
-                if len(res) == k:                  # stop early
-                    return res
+        
+        ans = []
+        for val in range(len(buckets) - 1, 0, -1):
+            for num in buckets[val]:
+                ans.append(num)
+                if len(ans) == k:
+                    return ans
